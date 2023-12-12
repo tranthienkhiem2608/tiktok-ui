52f762ecf203cbfe2f729f42cdec2668d7390282
Your Name <you@example.com>
2023-12-12 16:47:19 +0700

    Complete Header & SideBar of Tiktok App

---
+++ src/components/SuggestedAccounts/AccountItem.js
@@ -0,0 +1,47 @@
+import classNames from 'classnames/bind';
+import Tippy from '@tippyjs/react/headless';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import styles from './SuggestedAccounts.module.scss';
+import AccountPreview from './AccountPreview';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    const renderPreview = (props) => {
+        return (
+            <div tabIndex="-1" {...props}>
+                <PopperWrapper>
+                    <AccountPreview />
+                </PopperWrapper>
+            </div>
+        );
+    };
+
+    return (
+        <div>
+            <Tippy interactive delay={[800, 0]} offset={[-20, 0]} placement="bottom" render={renderPreview}>
+                <div className={cx('account-item')}>
+                    <img
+                        className={cx('avatar')}
+                        src="https://bizweb.dktcdn.net/100/438/408/files/meme-meo-cute-yody-vn-4.jpg?v=1690276115810"
+                        alt=""
+                    />
+                    <div className={cx('item-info')}>
+                        <p className={cx('nickname')}>
+                            <strong>quocnguyenphu</strong>
+                            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                        </p>
+                        <p className={cx('name')}>Quốc Nguyễn Phú</p>
+                    </div>
+                </div>
+            </Tippy>
+        </div>
+    );
+}
+
+AccountItem.propTypes = {};
+
+export default AccountItem;
--- src/layouts/DefaultLayout/Sidebar/index.js
+++ src/layouts/DefaultLayout/Sidebar/index.js
@@ -1,12 +1,34 @@
 import classNames from 'classnames/bind';
 import styles from './Sidebar.module.scss';
+import Menu, { MenuItem } from './Menu';
+import {
+    HomeIcon,
+    HomeActiveIcon,
+    UserGroupIcon,
+    UserGroupActiveIcon,
+    LiveIcon,
+    LiveActiveIcon,
+} from '~/components/Icons';
+import config from '~/config';
+import SuggestedAccounts from '~/components/SuggestedAccounts';
 
 const cx = classNames.bind(styles);
 
 function Sidebar() {
     return (
         <aside className={cx('wrapper')}>
-            <h2>Sidebar</h2>
+            <Menu>
+                <MenuItem title="For You" to={config.routes.home} icon={<HomeIcon />} activeIcon={<HomeActiveIcon />} />
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupActiveIcon />}
+                />
+                <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
+            </Menu>
+            <SuggestedAccounts label="Suggested accounts" />
+            <SuggestedAccounts label="Following accounts" />
         </aside>
     );
 }
